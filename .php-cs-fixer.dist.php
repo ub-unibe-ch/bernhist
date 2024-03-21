6a64e341fad956d86f603606fdb36b50af312e17
kevin-studer <kevin.studer@unibe.ch>
2024-03-21 14:33:05 +0000

    Add cs fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,19 @@
+<?php 
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->exclude('node_modules')
+    ->exclude('vendor')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PhpCsFixer' => true,
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'php_unit_test_class_requires_covers' => false,
+    ])
+    ->setRiskyAllowed(true)
+    ->setFinder($finder)
+;
