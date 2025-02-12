8ffe66705207f3a57ce6001bb447f4ee30f1747f
j-at-ub <jasmin.cavelti@unibe.ch>
2025-02-12 10:16:58 +0100

    remove doctrine/annotations and use Attributes in Controllers

--- src/Controller/RenderController.php
+++ src/Controller/RenderController.php
@@ -8,7 +8,7 @@ use App\Repository\LocationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(path: '/render')]
 class RenderController extends AbstractController
