b5d41f6529fe30f18f868d8528a11a07cc2a71e7
Mathias Stocker <mathias.stocker@unibe.ch>
2025-09-18 17:35:50 +0200

    Update dependencies

--- tests/Controller/QueryControllerTest.php
+++ tests/Controller/QueryControllerTest.php
@@ -15,6 +15,9 @@ class QueryControllerTest extends WebTestCase
 
         $client->request('GET', '/query/');
 
+        file_put_contents('./var/test.html', $client->getResponse()->getContent());
+        exit;
+
         self::assertResponseIsSuccessful();
         self::assertSelectorTextContains('h1', 'Ortsauswahl');
         self::assertSelectorExists('#level-16');
