95550b64b821bcfdd391f56f2e3c3a37be1b7d8b
althausLuca <luca.althaus@unibe.ch>
2026-01-19 17:58:15 +0100

    classes updated for bs5 and document eventListeners updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,9 @@ const $ = require('jquery');
 require('bootstrap');
 require('./chartist-modal.js');
 
+
 $(document).ready(function(){
-   console.log('Welcome to BERNHIST!');
+   console.log('Welcome to BERNHIST!!');
    if(window.location.hash){
       let id = window.location.hash.substr(1);
       let current = $('#level-' + id);
